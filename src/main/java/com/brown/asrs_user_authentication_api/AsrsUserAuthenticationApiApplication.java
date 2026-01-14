39d5e5d47c07d15b0cbf6e73e7efd1d5c6c11079
Leeroy Brown <brownleeroy010@gmail.com>
2026-01-14 18:36:46 +0200

    initial commit

---
+++ src/main/java/com/brown/asrs_user_authentication_api/AsrsUserAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_user_authentication_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrsUserAuthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsrsUserAuthenticationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brown/asrs_user_authentication_api/AsrsUserAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brown.asrs_user_authentication_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrsUserAuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
